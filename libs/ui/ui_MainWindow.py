9bb63bcece9c81c4495ecca3afd8ef42ca27698e
captainfffsama <tuanzhangsama@outlook.com>
2023-01-11 15:57:31 +0800

    合并功能

--- libs/ui/ui_MainWindow.py
+++ libs/ui/ui_MainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/chiebotgpuhq/MyCode/python/labelP/labelp/libs/ui/MainWindow.ui'
+# Form implementation generated from reading ui file '/home/chiebotgpuhq/MyCode/python/label_homography/libs/ui/MainWindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
